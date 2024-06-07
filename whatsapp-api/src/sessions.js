020cd067fbcdbfb193588878bdc9708609047fa0
magdielcardoso <dzmartins.contato@icloud.com>
2024-06-07 13:26:01 -0300

    headless mode new

--- whatsapp-api/src/sessions.js
+++ whatsapp-api/src/sessions.js
@@ -122,7 +122,9 @@ const setupSession = (sessionId) => {
           '--single-process',
           '--disable-gpu'
         ],
-        headless: true,
+        headless: {
+          mode: 'new',
+        },
       },
       userAgent:
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
